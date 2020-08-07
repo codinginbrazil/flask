9a743e520c781f0feb33f6a5a764367bf84ab117
sswellington <wellingtons@id.uff.br>
2020-08-07 19:32:55 -0300

    test: routes

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -10,6 +10,17 @@ redis = Redis(host='redis', port=6379)
 
 
 @app.route("/")
+@app.route("/index")
 def index():
     redis.incr("hits")
     return "Flask is Fun %s time(s)" % redis.get("hits")
+
+
+@app.route("/welcome", defaults={'name': None})
+@app.route("/welcome/<name>")
+def welcome(name):
+    if name :
+        return "Welcome, %s" % name
+    else:
+        return "Welcome!"
+
